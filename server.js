6d888fbfd2574b1ef8351f39ef1d692b4f696d5e
Chris Taylor <chrismtaylor1998@gmail.com>
2021-04-16 08:39:10 -0400

    testing deployment

--- server.js
+++ server.js
@@ -15,29 +15,37 @@ const charlestonID = '4574324';
 //Humidity threshhold
 const humThresh = 70;
 
+// call to weather api with check and then call to twillio
+const apiCall = () => {
+    axios.get('http://api.openweathermap.org/data/2.5/weather', {
+        params: {
+            id: charlestonID,
+            appid: apiKey,
+            units: 'imperial'
+        }
+    })
+    .then(function (response) {
+        var humidity = response.data.main.humidity;
+        if (humidity < humThresh) {
+            client.messages.create({
+                body: `SPRAY NOSE! HUMIDITY IS ${humidity}`,
+                from: '***REMOVED***',
+                to: '***REMOVED***'
+            })
+            .then(message => console.log(message.sid))
+            .catch(error => console.log(error));
+        }
+        else { console.log('good to go chris!') }
+    })
+    .catch(error => console.log(error));
+}
+
+console.log('hello!!');
 
 
 
-axios.get('http://api.openweathermap.org/data/2.5/weather', {
-    params: {
-        id: charlestonID,
-        appid: apiKey,
-        units: 'imperial'
-    }
-})
-.then(function (response) {
-    var humidity = response.data.main.humidity;
-    if (humidity < humThresh){
-        client.messages
-        .create({
-           body: `SPRAY NOSE! HUMIDITY IS ${humidity}`,
-           from: '***REMOVED***',
-           to: '***REMOVED***'
-         })
-        .then(message => console.log(message.sid));
-    }
-    else { console.log('good to go chris!') }
-});
+
+
 
 
 
